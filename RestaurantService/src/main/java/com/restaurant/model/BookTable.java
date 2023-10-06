8fb86b77dea878949b6fce359727b0fae502d3a8
Narenderflora <86171954+Narenderflora@users.noreply.github.com>
2023-10-06 21:43:07 +0530

    Add files via upload

---
+++ RestaurantService/src/main/java/com/restaurant/model/BookTable.java
@@ -0,0 +1,60 @@
+package com.restaurant.model;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookTable {
+	private String bookingId;
+
+	@NotNull(message = "Provide value for customer name")
+	private String customerName;
+
+	private String mobileNumber;
+
+	private Integer totalGuestCount;
+
+	@DecimalMin(value = "0", inclusive = true, message = "Provide value greater than or equal to zero")
+	private Integer totalAdultVegCount;
+
+	@DecimalMin(value = "0", inclusive = true, message = "Provide value greater than or equal to zero")
+	private Integer totalAdultNonVegCount;
+
+	@DecimalMin(value = "0", inclusive = true, message = "Provide value greater than or equal to zero")
+	private Integer totalKidsVegCount;
+
+	@DecimalMin(value = "0", inclusive = true, message = "Provide value greater than or equal to zero")
+	private Integer totalKidsNonVegCount;
+
+	//@Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date must be in the format yyyy-MM-dd")
+	private LocalDate bookingDate;
+	private Double totalBillAmount;
+
+	public BookTable(String bookingId, String customerName, String mobileNumber, Integer totalGuestCount,
+			Integer totalAdultVegCount, Integer totalAdultNonVegCount, Integer totalKidsVegCount,
+			Integer totalKidsNonVegCount, LocalDate bookingDate, Double totalBillAmount) {
+		super();
+		this.bookingId = bookingId;
+		this.customerName = customerName;
+		this.mobileNumber = mobileNumber;
+		this.totalGuestCount = totalGuestCount;
+		this.totalAdultVegCount = totalAdultVegCount;
+		this.totalAdultNonVegCount = totalAdultNonVegCount;
+		this.totalKidsVegCount = totalKidsVegCount;
+		this.totalKidsNonVegCount = totalKidsNonVegCount;
+		this.bookingDate = bookingDate;
+		this.totalBillAmount = totalBillAmount;
+	}
+
+	public BookTable() {
+
+	}
+
+}
